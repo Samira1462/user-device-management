6fe31f4879b42d3f5a00e8e00fe6f9b208080f2c
samira <Samir.Radmaneshfar@gmail.com>
2023-10-03 10:44:54 +0200

    init commit

---
+++ src/main/kotlin/com/challang/userdevicemanagement/UserDeviceManagementApplication.kt
@@ -0,0 +1,11 @@
+package com.challang.userdevicemanagement
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class UserDeviceManagementApplication
+
+fun main(args: Array<String>) {
+	runApplication<UserDeviceManagementApplication>(*args)
+}
---
+++ src/test/kotlin/com/challang/userdevicemanagement/UserDeviceManagementApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.challang.userdevicemanagement
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class UserDeviceManagementApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
